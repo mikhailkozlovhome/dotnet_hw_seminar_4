a8c9dba6baed824961c23c8bf10b4797ca4e8207
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-02-26 13:57:26 +0300

    Add task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2:
+// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе. Реализовать через функции.
+
+int inputInt (string msg)
+{
+    int value;
+    System.Console.Write($"{msg} -> ");
+    if(int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Введенно неверное значение!");
+    Environment.Exit(999);
+    return 0;
+}
+int SumDigit(int number)
+{
+    int SumDig = 0;
+    while (number != 0)
+    {
+        SumDig += number % 10;
+        number /= 10;
+    }
+    if (SumDig >= 0)
+    {
+        return SumDig;
+    }
+    return SumDig * (-1);
+}
+
+int number = inputInt("Введите число");
+System.Console.WriteLine($"Сумма цифр в числе {number} равна {SumDigit(number)}.");
