6646fc1043d95467c1d0d80f0fbe7c033ceba743
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-02-26 13:38:34 +0300

    Add task 1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1:
+// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B. Реализовать через функции.
+int inputInt (string msg)
+{
+    int value;
+    System.Console.Write($"{msg} -> ");
+    if(int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Введенно неверное значение!");
+    Environment.Exit(999);
+    return 0;
+}
+
+bool ValidatePower (int pow)
+{
+    if (pow <= 0)
+    {
+        System.Console.WriteLine($"Число {pow} не является натуральным!");
+        return false;
+    }
+    return true;
+}
+
+int power (int number, int pow)
+{
+    int result = 1;
+
+    for (int i = 1; i <= pow; i++)
+    {
+        result = result * number;
+    }
+
+    return result;
+}
+
+int A = inputInt("Введите число которое необходимо возвести в степень");
+int B = inputInt("Введите натуральную степень в которую необходимо возвести число");
+
+if (ValidatePower(B))
+{
+    System.Console.WriteLine($"Число {A} в степени {B} равно {power(A, B)}");
+}
